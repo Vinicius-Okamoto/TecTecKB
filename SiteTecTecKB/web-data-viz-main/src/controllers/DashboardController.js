6c0830bfe37dbcb980060446ba25bf447a682f61
Vinicius-Okamoto <vinicius.okamoto@sptech.school>
2025-11-22 15:32:02 -0300

    Atualizando caminho para enviar dados para o bd

--- SiteTecTecKB/web-data-viz-main/src/controllers/DashboardController.js
+++ SiteTecTecKB/web-data-viz-main/src/controllers/DashboardController.js
@@ -12,24 +12,57 @@ function listar(req, res) {
 
 function cadastrar(req, res) {
     var idade = req.body.idadeServer;
-    var idade = req.body.somServer;
-    var idade = req.body.experienciaServer;
-    var idade = req.body.comprarServer;
-    var idade = req.body.motivoServer;
-    var idade = req.body.customizarServer;
-    var idade = req.body.elementosServer;
-    var idade = req.body.layoutServer;
-    var idade = req.body.tendenciaServer;
+    var som = req.body.somServer;
+    var experiencia = req.body.experienciaServer;
+    var comprar = req.body.comprarServer;
+    var motivo = req.body.motivoServer;
+    var customizar = req.body.customizarServer;
+    var elementos = req.body.elementosServer;
+    var layout = req.body.layoutServer;
+    var tendencia = req.body.tendenciaServer;
     
     if (idade == undefined) {
-        res.status(400).send("Seu nome está undefined!");
-    }
-
-    DashboardModel.cadastrar(idade).then(function(resposta){
-        res.status(200).send("Carro criado com sucesso");
-    }).catch(function(erro){
-        res.status(500).json(erro.sqlMessage);
-    })
+        res.status(400).send("Sua idade está undefined!");
+    } else if (som == undefined) {
+        res.status(400).send("O resultado som está undefined!");
+    } else if (experiencia == undefined) {
+        res.status(400).send("O resultado experiencia está undefined!");
+    } else if (comprar == undefined) {
+        res.status(400).send("O resultado comprar está undefined!");
+    } else if (motivo == undefined) {
+        res.status(400).send("O resultado motivo está undefined!");
+    } else if (customizar == undefined) {
+        res.status(400).send("O resultado customizar está undefined!");
+    } else if (elementos == undefined) {
+        res.status(400).send("O resultado elementos está undefined!");
+    } else if (layout == undefined) {
+        res.status(400).send("O resultado layout está undefined!");
+    } else if (tendencia == undefined) {
+        res.status(400).send("O resultado tendencia está undefined!");
+    } else {
+    
+            // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+            DashboardModel.cadastrar(som, experiencia, comprar, motivo, customizar, elementos, layout, tendencia, idade)
+                .then(
+                    function (resultado) {
+                        res.json(resultado);
+                    }
+                ).catch(
+                    function (erro) {
+                        console.log(erro);
+                        console.log(
+                            "\nHouve um erro ao realizar o cadastro! Erro: ",
+                            erro.sqlMessage
+                        );
+                        res.status(500).json(erro.sqlMessage);
+                    }
+                );
+        }
+    // DashboardModel.cadastrar(idade).then(function(resposta){
+    //     res.status(200).send("Carro criado com sucesso");
+    // }).catch(function(erro){
+    //     res.status(500).json(erro.sqlMessage);
+    // })
 }
 
 module.exports = {
--- SiteTecTecKB/web-data-viz-main/src/models/DashboardModel.js
+++ SiteTecTecKB/web-data-viz-main/src/models/DashboardModel.js
@@ -8,9 +8,9 @@ function listar() {
     return database.executar(instrucao);
 }
 
-function cadastrar(idade) {
+function cadastrar(som, experiencia, comprar, motivo, customizar, elementos, layout, tendencia, idade) {
     var instrucao = `
-        INSERT INTO respostas (resposta1, resposta2, resposta3, resposta4, resposta5, resposta6, resposta7, resposta8, usuarioId, idade) VALUES (${},  ${}, ${}, ${}, ${}, ${}, ${}, ${}, ${idade}');
+        INSERT INTO respostas (resposta1, resposta2, resposta3, resposta4, resposta5, resposta6, resposta7, resposta8, idade) VALUES ('${som}', '${experiencia}', '${comprar}', '${motivo}', '${customizar}', '${elementos}', '${layout}', '${tendencia}', '${idade}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
