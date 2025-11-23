3db2bd1b187da26086558642ce2c59971f7f9c3a
Vinicius-Okamoto <vinicius.okamoto@sptech.school>
2025-11-23 03:42:55 -0300

    Atualizando fetch para input de dados

--- SiteTecTecKB/web-data-viz-main/src/controllers/DashboardController.js
+++ SiteTecTecKB/web-data-viz-main/src/controllers/DashboardController.js
@@ -2,10 +2,12 @@ var DashboardModel = require("../models/DashboardModel");
 
 
 function listar(req, res) {
-    DashboardModel.listar().then(function(resultado){
+    DashboardModel.listar(idade).then(function(response){
         // precisamos informar que o resultado voltará para o front-end como uma resposta em json
-        res.status(200).json(resultado);
+        res.status(200).json(response);
     }).catch(function(erro){
+        console.log(erro);
+        console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
         res.status(500).json(erro.sqlMessage);
     })
 }
