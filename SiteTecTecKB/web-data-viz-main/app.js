24c3e3b6acd2d33afadaa102ab216fd8e8c4af5a
Vinicius-Okamoto <vinicius.okamoto@sptech.school>
2025-11-30 18:38:25 -0300

    Atualizando versão limpa e com menos erros

--- SiteTecTecKB/web-data-viz-main/app.js
+++ SiteTecTecKB/web-data-viz-main/app.js
@@ -18,7 +18,6 @@ var app = express();
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
 var avisosRouter = require("./src/routes/avisos");
-var medidasRouter = require("./src/routes/medidas");
 var DashboardRouter = require("./src/routes/Dashboard.js");
 
 
@@ -31,7 +30,6 @@ app.use(cors());
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 app.use("/avisos", avisosRouter);
-app.use("/medidas", medidasRouter);
 app.use("/Dashboard", DashboardRouter);
 
 
--- SiteTecTecKB/web-data-viz-main/src/controllers/usuarioController.js
+++ SiteTecTecKB/web-data-viz-main/src/controllers/usuarioController.js
@@ -18,22 +18,14 @@ function autenticar(req, res) {
 
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
-                        res.status(200).json({mensagem:'Login Efetuado com sucesso', resultadoAutenticar})
-
-                            // fazer agora
-                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                            .then((resultadoAquarios) => {
-                                if (resultadoAquarios.length > 0) {
-                                    res.json({
-                                        id: resultadoAutenticar[0].id,
-                                        email: resultadoAutenticar[0].email,
-                                        nome: resultadoAutenticar[0].nome,
-                                        senha: resultadoAutenticar[0].senha,
-                                    });
-                                } else {
-                                    res.status(204).json({ aquarios: [] });
-                                }
-                            })
+                        // res.status(200).json({mensagem:'Login Efetuado com sucesso', resultadoAutenticar});
+                        
+                        res.json({
+                            id: resultadoAutenticar[0].id,
+                            nome: resultadoAutenticar[0].nome,
+                            email: resultadoAutenticar[0].email,
+                            // senha: resultadoAutenticar[0].senha,
+                        });
 
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
